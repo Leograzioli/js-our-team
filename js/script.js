033e045a5887af935c1df12ddf8f62971559df49
Leograzioli <leograzioli@gmail.com>
2022-10-20 13:50:50 +0200

    Milestone 2

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const myTeamArray = [
+    {
+    name:"Wayne Barnett",
+    role: "Founder & CEO",
+    imgProfile: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+    name:"Angela Caroll",
+    role: "Chief Editor",
+    imgProfile: "angela-caroll-chief-editor.jpg"
+    },
+    {
+    name:"Walter Gordon",
+    role: "Office Manager",
+    imgProfile: "walter-gordon-office-manager.jpg"
+    },
+    {
+    name:"Angela Lopez",
+    role: "Social Media Manager",
+    imgProfile: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+    name:"Scott Estrada",
+    role: "Developer",
+    imgProfile: "scott-estrada-developer.jpg"
+    },
+    {
+    name:"Barbara Ramos",
+    role: "Graphic Designer",
+    imgProfile: "barbara-ramos-graphic-designer.jpg"
+    }
+];
+
+const teamList = document.getElementById("team-list");
+for (let i = 0; i < myTeamArray.length; i++) {
+    const element = myTeamArray[i];
+
+    for (let key in element) {
+        console.log(key, element[key]);       
+    }
+
+    teamList.innerHTML += `<li>Name:${element["name"]}  <br> Role:${element.role} <br> Img: ${element.imgProfile}</li>`
+}
